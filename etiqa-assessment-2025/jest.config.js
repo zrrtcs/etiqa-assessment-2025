2655587a41ab3448025735954d6b1e8841687e18
zrrtcs <aizzat.suhardi@gmail.com>
2025-11-10 16:17:56 +0800

    Clean up unused config and asset files

--- etiqa-assessment-2025/jest.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx'],
-  testMatch: ['**/__tests__/**/*.js', '**/*.test.js'],
-  collectCoverageFrom: ['src/**/*.js', '!src/main.jsx'],
-};
